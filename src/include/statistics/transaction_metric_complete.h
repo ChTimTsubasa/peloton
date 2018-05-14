d736eb494d9489f73270b2f108c5cc44d8fd8d90
Tianyu Li <tli2@andrew.cmu.edu>
2018-05-13 21:06:18 -0400

    Change template

--- src/include/statistics/transaction_metric_complete.h
+++ src/include/statistics/transaction_metric_complete.h
@@ -30,9 +30,24 @@ class TransactionMetricRawData : public AbstractRawData {
   }
 
   void Aggregate(AbstractRawData &other) override {
+    auto &other_data = dynamic_cast<TransactionMetricRawData &>(other);
+    Concat(txns_, other_data.txns_);
+    Concat(counts_, other_data.counts_);
+    Concat(durations_, other_data.durations_);
+    Concat(commits_, other_data.commits_);
   }
 
   void UpdateAndPersist() override {
+    auto txn =
+        concurrency::TransactionManagerFactory::GetInstance().BeginTransaction();
+    auto catalog = catalog::TransactionMetricsCatalog::GetInstance(txn);
+    for (size_t i = 0; i < txns_.size(); i++)
+      catalog->InsertTxnMetric(txns_[i],
+                               counts_[i],
+                               durations_[i],
+                               commits_[i],
+                               nullptr,
+                               txn);
   }
 
  private:
@@ -50,19 +65,31 @@ class TransactionMetricRawData : public AbstractRawData {
 class TransactionMetric : public AbstractMetric<TransactionMetricRawData> {
  public:
   void OnTransactionBegin(const concurrency::TransactionContext *context) override {
+    timers_[context->GetTransactionId()].Start();
   }
 
   void OnTransactionCommit(const concurrency::TransactionContext *context, oid_t) override {
+    WriteEntry(context->GetTransactionId(), true);
   }
 
   void OnTransactionAbort(const concurrency::TransactionContext *context, oid_t) override {
+    WriteEntry(context->GetTransactionId(), false);
   }
 
   void OnTupleRead(const concurrency::TransactionContext *context, oid_t) override {
+    counts_[context->GetTransactionId()]++;
   }
 
  private:
   void WriteEntry(txn_id_t tid, bool committed) {
+    auto raw_data = GetRawData();
+    timers_[tid].Stop();
+    raw_data->txns_.push_back(tid);
+    raw_data->counts_.push_back(counts_[tid]);
+    raw_data->durations_.push_back(timers_[tid].GetDuration());
+    raw_data->commits_.push_back(committed);
+    counts_.erase(tid);
+    timers_.erase(tid);
   }
 
   std::unordered_map<txn_id_t, uint64_t> counts_;
